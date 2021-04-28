9f0da5b3885415e6d80b63d68fc88593e3b980a5
Bobby <bhshi@utexas.edu>
2021-04-28 11:40:05 -0500

    add ensemble

---
+++ sparse_rf/algs/ensemble.py
@@ -0,0 +1,6 @@
+import numpy as np
+from sparse_rf.algs.core import *
+from sparse_rf.util.dists import *
+from sparse_rf.modules.matrix import *
+from sparse_rf.util.activations import *
+    
--- sparse_rf/util/__init__.py
+++ sparse_rf/util/__init__.py
@@ -1,3 +1,2 @@
-from sparse_rf.util.algs import *
 from sparse_rf.util.dists import *
 from sparse_rf.util.activations import *

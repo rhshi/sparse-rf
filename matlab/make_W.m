90d42ac87b5cab0ad13a66afea8fb8f30fbf8143
Bobby <bshi1324@gmail.com>
2021-05-22 17:18:24 -0500

    add inds_track

--- matlab/make_W.m
+++ matlab/make_W.m
@@ -1,8 +1,9 @@
-function [W, Nreal] = make_W(d, q, N, scale)
+function [W, Nreal, inds_track] = make_W(d, q, N, scale)
     num_supports = nchoosek(d, q);
     if num_supports > N
         Nreal = N;
         W = zeros(Nreal, d);
+        inds_track = zeros(num_supports, q);
         for i = 1:Nreal
             w = rand(1, d) * scale;
             inds = randsample(d, d-q);
@@ -10,12 +11,14 @@ function [W, Nreal] = make_W(d, q, N, scale)
             W(i, :) = w;
         end
     else
-        n = round(N/num_supports);
+        n = round(N / num_supports);
         Nreal = n * num_supports;
         inds = nchoosek(1:d, d-q);
-        W = zeros(Nreal, d);
+        W = zeros(n*num_supports, d);
+        inds_track = zeros(num_supports, q);
         for i = 1:num_supports
             ind = inds(i, :);
+            inds_track(i, :) = setdiff(1:d, ind);
             for j = 1:n
                 w = randn(1, d) * scale;
                 w(ind) = 0;

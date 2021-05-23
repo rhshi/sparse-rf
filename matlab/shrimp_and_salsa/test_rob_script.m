a0bf7200f53c2532986f7a10949279c4c021a554
Yuege (Gail) Xie <yuege@ices.utexas.edu>
2021-05-22 23:00:35 -0500

    fix bugs for showing results

--- matlab/shrimp_and_salsa/test_rob_script.m
+++ matlab/shrimp_and_salsa/test_rob_script.m
@@ -8,16 +8,18 @@ data_list = {'propulsion', 'galaxy', 'airfoil', 'speech', 'CCPP', ...
     'telemonitoring-total', 'blog'};
 
 % data_list = {'propulsion'};
-per_list =  [0.1, 0.15, 0.2, 0.25, 0.3, 0.45, 0.4, 0.45, 0.5, 0.55, 0.6];
+per_list =  [0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5];
 mse_dict = robust_test(data_list, per_list);
 
-tiledlayout(length(data_list),1)
+tiledlayout(3,4)
 for i = 1:length(data_list)
     nexttile
-    plot(per_list, mse_dict(data_list{i}))
+    err = mse_dict(data_list{i});
+    scatter(per_list, err, 'blue')
+    scatter(per_list, err, 'blue')
     title(data_list{i})
     xlabel("pruning percentage")
     ylabel("Test MSE")
 end
 
-save(".results/robust_test", 'mse_dict');
+save("./results/robust_test", 'mse_dict');
